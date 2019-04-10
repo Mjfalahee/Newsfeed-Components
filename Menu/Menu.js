dffb8a4b3494284e199ff7421db902d90e47819b
mjfalahee <mjfalahee@gmail.com>
2019-04-10 13:37:44 -0700

    Working on Article.js expansion method

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -15,6 +15,9 @@ const menuButton = document.querySelector('.menu-button')
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
 menuButton.addEventListener('click', () => toggleMenu())
+
+
+
 // CODE DUMP
 /*console.log('Menu Toggled!')
 this.
